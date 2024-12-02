840dca96d94250ccd5c1880db60c14ad0afb1ff8
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-02 22:00:01 +0300

    Creating a shell for development

---
+++ src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/SchoolProjectDataTransferProtocols.java
@@ -0,0 +1,17 @@
+package solutions.brilliant.schoolProjectDataTransferProtocols;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SchoolProjectDataTransferProtocols extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
