388a69986468c1b05fa3a75a9591c74e90a202e8
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-12 19:05:01 +0300

    Add sp command

--- src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/SchoolProjectDataTransferProtocols.java
+++ src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/SchoolProjectDataTransferProtocols.java
@@ -1,13 +1,15 @@
 package solutions.brilliant.schoolProjectDataTransferProtocols;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import solutions.brilliant.schoolProjectDataTransferProtocols.commands.SP;
 
 public final class SchoolProjectDataTransferProtocols extends JavaPlugin {
 
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
-
+        getCommand("sp").setExecutor(new SP());
     }
 
     @Override
