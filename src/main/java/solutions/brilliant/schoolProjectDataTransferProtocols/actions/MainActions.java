2ee610766f3c4bc055926ca7d32e0036109906c5
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-14 12:43:36 +0300

    Create class

---
+++ src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/actions/MainActions.java
@@ -0,0 +1,13 @@
+package solutions.brilliant.schoolProjectDataTransferProtocols.actions;
+
+import solutions.brilliant.schoolProjectDataTransferProtocols.connector.ConnectionTask;
+
+public class MainActions {
+
+    private final ConnectionTask task;
+
+    public MainActions(ConnectionTask task) {
+        this.task = task;
+    }
+
+}
