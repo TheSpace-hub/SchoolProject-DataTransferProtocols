00dd968337ff02341b7b759c54530516d32512de
Sergey Diachkov <78466469+TheSpace-hub@users.noreply.github.com>
2024-12-14 15:12:30 +0300

    Actions

--- src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/SchoolProjectDataTransferProtocols.java
+++ src/main/java/solutions/brilliant/schoolProjectDataTransferProtocols/SchoolProjectDataTransferProtocols.java
@@ -2,6 +2,7 @@ package solutions.brilliant.schoolProjectDataTransferProtocols;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import solutions.brilliant.schoolProjectDataTransferProtocols.actions.MainActions;
 import solutions.brilliant.schoolProjectDataTransferProtocols.commands.SP;
 import solutions.brilliant.schoolProjectDataTransferProtocols.connector.ConnectionTask;
 import solutions.brilliant.schoolProjectDataTransferProtocols.connector.Connector;
@@ -13,8 +14,9 @@ public final class SchoolProjectDataTransferProtocols extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
         Connector connector = new Connector(25570, "127.0.0.1");
+        MainActions actions = new MainActions(this);
+        ConnectionTask task = new ConnectionTask(actions, connector);
 
-        ConnectionTask task = new ConnectionTask(connector);
         Bukkit.getScheduler().runTaskTimer(this, task, 1L, 1L);
 
         getCommand("sp").setExecutor(new SP(task));
